8c65500b2fda4b937b653e6053b71e73b436e2f9
Fadhli <mfadhliismail@gmail.com>
2020-04-16 10:56:54 +0100

    updating config and package.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,13 +12,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content`,
-        ignore: process.env.NODE_ENV === `production` && [`**/draft*`],
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         path: `${__dirname}/content/blog`,
         name: `blog`,
       },
